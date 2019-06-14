f4a9e8676acf71487fbac34922b95690019f9da1
harshitjindal <harshit.jindal@icloud.com>
2019-06-14 10:56:36 +0530

    Initial Commit

---
+++ Location App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Location App
+//
+//  Created by Harshit Jindal on 14/06/19.
+//  Copyright © 2019 Harshit Jindal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
